e2bf7a85aa3f8437d3f018731ceaee80ed433cd4
Cisa Valentino <118332009+cisavalentino@users.noreply.github.com>
2025-01-23 09:40:48 +0700

    Update TwoSum.py

--- ProblemList/TwoSum/TwoSum.py
+++ ProblemList/TwoSum/TwoSum.py
@@ -1,1 +1,12 @@
+from typing import List
+nums = [2,7,11,15]
+target = 22
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[num] = i
